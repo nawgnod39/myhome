afc0d088e5ccec34b0615d5a446feebb7ac4798a
dongwantak <dongwantak@gmail.com>
2021-05-07 02:22:13 +0900

    layout 구성

---
+++ src/main/java/com/takdong/myhome/controller/BoardController.java
@@ -0,0 +1,17 @@
+package com.takdong.myhome.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+
+    @GetMapping("/list")
+    public String list() {
+        return "board/list";//templates 의  board/list.html 을 찾아감 .
+    }
+
+}
